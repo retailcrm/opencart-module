d860f6203ce60f7f34006acfdae55cfff9ce7e51
iyzoer <akolzinvs@gmail.com>
2017-05-18 17:29:28 +0300

    upload in CRM of the customer's address after registration, upload updates customers

--- catalog/controller/extension/module/retailcrm.php
+++ catalog/controller/extension/module/retailcrm.php
@@ -53,6 +53,13 @@ class ControllerExtensionModuleRetailcrm extends Controller
         }
     }
 
+    /**
+     * Update order on event
+     *
+     * @param int $order_id order identificator
+     *
+     * @return void
+     */
     public function order_edit($parameter1, $parameter2 = null) {
         $order_id = $parameter2[0];
 
@@ -94,12 +101,48 @@ class ControllerExtensionModuleRetailcrm extends Controller
      * @return void
      */
     public function customer_create($parameter1, $parameter2 = null, $parameter3 = null) {
+        $this->load->model('account/customer');
+        $this->load->model('localisation/country');
+        $this->load->model('localisation/zone');
+
         $customerId = $parameter3;
+        $customer = $this->model_account_customer->getCustomer($customerId);
+
+        if ($this->request->post) {
+            $country = $this->model_localisation_country->getCountry($this->request->post['country_id']);
+            $zone = $this->model_localisation_zone->getZone($this->request->post['zone_id']);
+
+            $customer['address'] = array(
+                'address_1' => $this->request->post['address_1'],
+                'address_2' => $this->request->post['address_2'],
+                'city' => $this->request->post['city'],
+                'postcode' => $this->request->post['postcode'],
+                'iso_code_2' => $country['iso_code_2'],
+                'zone' => $zone['name']
+            );
+        }
+
+        $this->load->model('extension/retailcrm/customer');
+        $this->model_extension_retailcrm_customer->sendToCrm($customer);
+    }
+
+    /**
+     * Update customer on event
+     *
+     * @param int $customerId customer identificator
+     *
+     * @return void
+     */
+    public function customer_edit($parameter1, $parameter2, $parameter3) {
+        $customerId = $this->customer->getId();
 
         $this->load->model('account/customer');
         $customer = $this->model_account_customer->getCustomer($customerId);
 
+        $this->load->model('account/address');
+        $customer['address'] = $this->model_account_address->getAddress($customer['address_id']);
+
         $this->load->model('extension/retailcrm/customer');
-        $this->model_extension_retailcrm_customer->sendToCrm($customer);
+        $this->model_extension_retailcrm_customer->changeInCrm($customer);
     }
 }
