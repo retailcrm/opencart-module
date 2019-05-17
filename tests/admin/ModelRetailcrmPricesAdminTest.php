7cbd322ad4446179b3b91b11ca56efb9a790caf3
Sergey <gluschenko@retailcrm.ru>
2019-05-17 17:25:06 +0300

    fix test

--- tests/admin/ModelRetailcrmPricesAdminTest.php
+++ tests/admin/ModelRetailcrmPricesAdminTest.php
@@ -58,9 +58,9 @@ class ModelRetailcrmPricesAdminTest extends OpenCartTest
         $this->assertSame('special1', $price['prices'][0]['code']);
         $this->assertSame('special2', $price['prices'][1]['code']);
         $this->assertSame('special3', $price['prices'][2]['code']);
-        $this->assertFalse($price['prices'][0]['price']);
-        $this->assertFalse($price['prices'][1]['price']);
-        $this->assertNotFalse($price['prices'][2]['price']);
+        $this->assertFalse($price['prices'][0]['remove']);
+        $this->assertFalse($price['prices'][1]['remove']);
+        $this->assertNotFalse($price['prices'][2]['remove']);
     }
 
     private function getSites()
