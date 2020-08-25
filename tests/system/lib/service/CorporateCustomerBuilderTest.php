6361a3651048bbaf13de6205bee068b5e1bfeb78
Павел <pashok9825@gmail.com>
2020-08-25 12:42:31 +0300

    tests

--- tests/system/lib/service/CorporateCustomerBuilderTest.php
+++ tests/system/lib/service/CorporateCustomerBuilderTest.php
@@ -21,17 +21,28 @@ class CorporateCustomerBuilderTest extends TestCase {
             'shipping_address_2' => ''
         );
 
-        $corp = \retailcrm\service\CorporateCustomerBuilder::create()
+        $builder = \retailcrm\service\CorporateCustomerBuilder::create();
+        $corp = $builder
             ->setCustomerExternalId(1)
             ->setCompany('Company')
             ->addCompany($data)
             ->addAddress($data)
             ->build();
+        $corp_with_is_main = $builder->setIsMainCompany(true)->build();
+        $corp_with_address = $builder->setCompanyAddressId(12)->build();
 
-        $this->assertNotEmpty($corp);
-        $this->assertNotEmpty($corp['addresses']);
-        $this->assertNotEmpty($corp['companies']);
-        $this->assertNotEmpty($corp['customerContacts'][0]['customer']);
-        $this->assertEquals(1, $corp['customerContacts'][0]['customer']['externalId']);
+        self::assertNotEmpty($corp);
+        self::assertNotEmpty($corp['addresses']);
+        self::assertNotEmpty($corp['companies']);
+        self::assertFalse(isset($corp['companies'][0]['isMain']));
+        self::assertArrayHasKey('isMain', $corp_with_is_main['companies'][0]);
+        self::assertTrue($corp_with_is_main['companies'][0]['isMain']);
+        self::assertArrayHasKey('address', $corp_with_address['companies'][0]);
+        self::assertNotEmpty($corp_with_address['companies'][0]['address']);
+        self::assertEquals(12, $corp_with_address['companies'][0]['address']['id']);
+        self::assertNotEmpty($corp['customerContacts'][0]['customer']);
+        self::assertEquals(1, $corp['customerContacts'][0]['customer']['externalId']);
+
+
     }
 }
