9312a6c6abc7a4d798ec58574a75e8f753fc732a
ellynoize <na1ton.trik@gmail.com>
2025-07-08 12:52:56 +0300

    hide collector

--- src/upload/system/library/retailcrm/retailcrm.php
+++ src/upload/system/library/retailcrm/retailcrm.php
@@ -17,7 +17,7 @@ class Retailcrm {
 
     const RETAILCRM_DISCOUNT = 'retailcrm_discount';
     const RETAILCRM_DISCOUNT_SORT_ORDER = 8;
-    const VERSION_MODULE = '4.2.0';
+    const VERSION_MODULE = '4.2.1';
 
     protected $registry;
 
